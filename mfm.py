16f6894d55c0b07549c546b908089eadceffa37e
Luigi Cruz <luigifcruz@gmail.com>
2020-09-21 02:11:43 -0300

    Update Compatibility With RC 0.2.2

--- mfm.py
+++ mfm.py
@@ -10,17 +10,14 @@ import sounddevice as sd
 
 # Demodulator Settings
 cuda = True
-freq = 97.5e6
+freq = 96.9e6
 tau = 75e-6
 sfs = int(256e3)
 afs = int(32e3)
-
 sdr_buff = 2048
-dsp_buff = sdr_buff * 16
-dsp_out = int(dsp_buff/(sfs/afs))
 
 # SoapySDR Configuration
-args = dict(driver="airspyhf")
+args = dict(driver="lime")
 sdr = SoapySDR.Device(args)
 sdr.setGainMode(SOAPY_SDR_RX, 0, True)
 sdr.setSampleRate(SOAPY_SDR_RX, 0, sfs)
@@ -28,22 +25,22 @@ sdr.setFrequency(SOAPY_SDR_RX, 0, freq)
 
 # Queue and Shared Memory Allocation
 que = queue.Queue()
-audio_file = open("FM_{}.if32".format(int(freq)), "bw")
 demod = MFM(tau, sfs, afs, cuda=cuda)
+audio_file = open("FM_{}.if32".format(int(freq)), "bw")
 
 # Declare the memory buffer
 if cuda:
     import cusignal as sig
-    buff = sig.get_shared_mem(dsp_buff, dtype=np.complex64)
+    buff = sig.get_shared_mem(sfs, dtype=np.complex64)
 else:
-    buff = np.zeros([dsp_buff], dtype=np.complex64)
+    buff = np.zeros([sfs], dtype=np.complex64)
 
 
 # Demodulation Function
 def process(outdata, f, t, s):
     outdata[:, 0] = demod.run(que.get())
     LPR = outdata[:, 0].astype(np.float32)
-    audio_file.write(LPR.tostring())
+    audio_file.write(LPR.tobytes())
 
 
 # Graceful Exit Handler
@@ -56,13 +53,13 @@ def signal_handler(signum, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # Start Collecting Data
-plan = [(i*sdr_buff) for i in range(dsp_buff//sdr_buff)]
+plan = [(i*sdr_buff) for i in range(sfs//sdr_buff)]
 rx = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
 sdr.activateStream(rx)
 
-with sd.OutputStream(blocksize=dsp_out, callback=process,
+with sd.OutputStream(blocksize=afs, callback=process,
                      samplerate=afs, channels=1):
     while True:
         for i in plan:
-            sdr.readStream(rx, [buff[i:]], sdr_buff)
-        que.put_nowait(buff.copy())
+            sdr.readStream(rx, [buff[i:]], sdr_buff, timeoutUs=int(1e9))
+        que.put_nowait(buff.astype(np.complex64))
--- multi_wbfm.py
+++ multi_wbfm.py
@@ -2,18 +2,20 @@
 
 from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
 import SoapySDR
-import pyaudio
 import signal
 import queue
 import numpy as np
 from radio.analog import WBFM
 from radio.tools import Tuner
 import cusignal as sig
+import sounddevice as sd
 
 # Demodulator Settings
+cuda = True
 tau = 75e-6
 sfs = int(240e3)
 afs = int(48e3)
+sdr_buff = 2400
 
 radios = [
     { "freq": 97.5e6, "bw": sfs },
@@ -22,20 +24,15 @@ radios = [
 
 # Queue and Shared Memory Allocation
 que = queue.Queue()
-p = pyaudio.PyAudio()
-
-tuner = Tuner(radios, sfs, cuda=True)
-demod = [ WBFM(tau, sfs, afs, sfs, cuda=True) for _ in radios ]
-sdr_buff = 1200
+tuner = Tuner(radios, sfs, cuda=cuda)
+demod = [ WBFM(tau, sfs, afs, cuda=cuda) for _ in radios ]
+afile = [ open("FM_{}.if32".format(int(f["freq"])), "bw") for f in radios ]
 
 print("# Tuner Settings:")
 print("     Bandwidth: {}".format(tuner.bw))
 print("     Mean Frequency: {}".format(tuner.mdf))
 print("     Offsets: {}".format(tuner.foff))
-print("     Radios: {}".format(len(radios)))
-
-# Create Recording Files
-afile = [ open("FM_{}.if32".format(int(f["freq"])), "bw") for f in radios ]
+print("     Stations: {}".format(len(radios)))
 
 # SoapySDR Configuration
 args = dict(driver="lime")
@@ -45,38 +42,29 @@ sdr.setSampleRate(SOAPY_SDR_RX, 0, tuner.bw)
 sdr.setFrequency(SOAPY_SDR_RX, 0, tuner.mdf)
 
 # Declare the memory buffer
-print("# CUDA Backend Enabled: (cuSignal + Cupy)")
-buff = sig.get_shared_mem(tuner.size, dtype=np.complex64)
+if cuda:
+    import cusignal as sig
+    buff = sig.get_shared_mem(tuner.size, dtype=np.complex64)
+else:
+    buff = np.zeros([tuner.size], dtype=np.complex64)
 
 
 # Demodulation Function
-def process(in_data, frame_count, time_info, status):
+def process(outdata, f, t, s):
     tuner.load(que.get())
 
     for i, _ in enumerate(radios):
-        buffer = tuner.run(i)
-        L, R = demod[i].run(buffer)
-        LR = np.ravel(np.column_stack((L, R))).astype(np.float32)
-        afile[i].write(LR.tostring('C'))
+        L, R = demod[i].run(tuner.run(i))
+        LR = np.ravel(np.column_stack((L, R)))
+        LR = LR.astype(np.float32)
+        afile[i].write(LR.tobytes())
 
-    LR = LR.reshape(demod[0].out, 2)
-    return (LR, pyaudio.paContinue)
-
-
-stream = p.open(
-    format=pyaudio.paFloat32,
-    channels=2,
-    frames_per_buffer=demod[0].out,
-    rate=afs,
-    output=True,
-    stream_callback=process)
+    outdata[:] = np.dstack((L, R))
 
 
 # Graceful Exit Handler
 def signal_handler(signum, frame):
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    sdr.deactivateStream(rx)
     sdr.closeStream(rx)
     exit(-1)
 
@@ -84,11 +72,13 @@ def signal_handler(signum, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # Start Collecting Data
-stream.start_stream()
+plan = [(i*sdr_buff) for i in range(tuner.size//sdr_buff)]
 rx = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
 sdr.activateStream(rx)
 
-while True:
-    for i in range(tuner.size//sdr_buff):
-        sdr.readStream(rx, [buff[(i*sdr_buff):]], sdr_buff, timeoutUs=int(1e9))
-    que.put(buff.astype(np.complex64))
+with sd.OutputStream(blocksize=afs, callback=process,
+                     samplerate=afs, channels=2):
+    while True:
+        for i in plan:
+            sdr.readStream(rx, [buff[i:]], sdr_buff, timeoutUs=int(1e9))
+        que.put(buff.astype(np.complex64))
