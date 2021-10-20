6cb9de5ed46342e317e29b9d2656f32841038901
Luigi Cruz <luigifcruz@gmail.com>
2021-10-20 15:07:28 -0300

    update wbfm example

--- wbfm.py
+++ wbfm.py
@@ -1,68 +1,61 @@
-#/usr/bin/env python3
-
-from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
-import SoapySDR
-import signal
-import queue
+import sys
 import numpy as np
-from radio.analog import WBFM
 import sounddevice as sd
-
-# Demodulator Settings
-cuda = True
-freq = 96.9e6
-tau = 75e-6
-sfs = int(240e3)
-afs = int(48e3)
-
-sdr_buff = 2400
-dsp_buff = sdr_buff * 10
-dsp_out = int(dsp_buff/(sfs/afs))
-
-# SoapySDR Configuration
-args = dict(driver="lime")
-sdr = SoapySDR.Device(args)
-sdr.setGainMode(SOAPY_SDR_RX, 0, True)
-sdr.setSampleRate(SOAPY_SDR_RX, 0, sfs)
-sdr.setFrequency(SOAPY_SDR_RX, 0, freq)
-
-# Queue and Shared Memory Allocation
-que = queue.Queue()
-audio_file = open("FM_{}.if32".format(int(freq)), "bw")
-demod = WBFM(tau, sfs, afs, cuda=cuda)
-
-# Declare the memory buffer
-if cuda:
-    import cusignal as sig
-    buff = sig.get_shared_mem(dsp_buff, dtype=np.complex64)
-else:
-    buff = np.zeros([dsp_buff], dtype=np.complex64)
+from radiocore import WBFM, Buffer, Carrousel, Chopper
+from SoapySDR import Device, SOAPY_SDR_RX, SOAPY_SDR_CF32
 
 
-# Demodulation Function
-def process(outdata, f, t, s):
-    outdata[:] = np.dstack(demod.run(que.get()))
-    LPR = outdata[:].astype(np.float32)
-    audio_file.write(LPR.tobytes())
+def process(outdata, *_):
+    if not carsl.is_healthy:
+        return
+
+    # Load, demod, and play buffer.
+    with carsl.dequeue() as buffer:
+        demoded = demod.run(buffer)
+        outdata[:] = np.dstack(demoded)
 
 
-# Graceful Exit Handler
-def signal_handler(signum, frame):
-    sdr.deactivateStream(rx)
-    sdr.closeStream(rx)
-    exit(-1)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-# Start Collecting Data
-plan = [(i*sdr_buff) for i in range(dsp_buff//sdr_buff)]
-rx = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
-sdr.activateStream(rx)
-
-with sd.OutputStream(blocksize=dsp_out, callback=process,
-                     samplerate=afs, channels=2):
+def receive(carsl, device_buffer):
     while True:
-        for i in plan:
-            sdr.readStream(rx, [buff[i:]], sdr_buff, timeoutUs=int(1e9))
-        que.put_nowait(buff.copy())
+        # Load, fill, and enqueue buffer.
+        with carsl.enqueue() as buffer:
+            for chunk in chopr.chop(buffer):
+                sdr.readStream(rx, [chunk], device_buffer, timeoutUs=int(1e9))
+
+        # Start audio when buffer reaches N samples.
+        if carsl.occupancy >= 4 and not stream.active:
+            stream.start()
+
+
+if __name__ == "__main__":
+    enable_cuda: bool = False
+    frequency: float = 96.9e6
+    deemphasis: float = 75e-6
+    input_rate: float = 256e3
+    output_rate: float = 32e3
+    device_buffer: float = 2048
+    device_name: str = "airspyhf"
+
+    # SoapySDR configuration.
+    sdr = Device({"driver": device_name})
+    sdr.setGainMode(SOAPY_SDR_RX, 0, True)
+    sdr.setSampleRate(SOAPY_SDR_RX, 0, input_rate)
+    sdr.setFrequency(SOAPY_SDR_RX, 0, frequency)
+    rx = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
+
+    # Queue and shared memory allocation.
+    carsl = Carrousel([Buffer(input_rate, cuda=enable_cuda) for _ in range(8)])
+    demod = WBFM(deemphasis, input_rate, output_rate, cuda=enable_cuda)
+    chopr = Chopper(input_rate, device_buffer)
+
+    # Start collecting data.
+    sdr.activateStream(rx)
+    stream = sd.OutputStream(blocksize=int(output_rate), callback=process,
+                             samplerate=int(output_rate), channels=2)
+
+    try:
+        receive(carsl, device_buffer)
+    except KeyboardInterrupt:
+        sdr.deactivateStream(rx)
+        sdr.closeStream(rx)
+        sys.exit('\nInterrupted by user')
